384704745568bc27444b4398e6767e526259f0de
ameyawebonise <ameya.kulkarni@weboniselab.com>
2019-06-27 17:17:26 +0530

    Changes for auto cancellationg

--- chapter5/goroutinecancellation/main.go
+++ chapter5/goroutinecancellation/main.go
@@ -9,23 +9,23 @@ import (
 func main() {
 	fmt.Println("Demo of cancellation of goroutine")
 	ctx, cancel := context.WithCancel(context.Background())
-
-	go longRunningWork(ctx)
-	time.Sleep(2 * time.Second)
-
-	cancel()
-	fmt.Scanln()
+	commChannel := make(chan struct{}, 1)
+	go longRunningWork(ctx, commChannel)
+	commChannel <- struct{}{}
+	time.Sleep(5 * time.Second)
+	defer cancel()
 }
 
-func longRunningWork(ctx context.Context) {
+func longRunningWork(ctx context.Context, reciever <-chan struct{}) {
 	fmt.Println("going to start doing some long running works")
 	for true {
 		select {
 		case <-ctx.Done():
 			fmt.Println("I am being asked to stop")
 			return
+		case <-reciever:
+			fmt.Println("Doing some long running work")
 		default:
-			fmt.Println("doing a long running work")
 		}
 	}
 	fmt.Println("Done the long running work")
