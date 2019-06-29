33db26349a71cfe8290ba0f9d7b9039f2596e900
kaddiya <kaddiya@gmail.com>
2019-06-29 14:55:23 +0530

    Race detector changes

--- chapter5/racedetector/main.go
+++ chapter5/racedetector/main.go
@@ -1,17 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	channel := make(chan bool)
 	sharedMap := make(map[string]string)
 	go func() {
 		sharedMap["1"] = "a" // First conflicting access.
-		channel <- true
 	}()
 	sharedMap["2"] = "b" // Second conflicting access.
-	<-channel
+
 	for k, v := range sharedMap {
 		fmt.Println(k, v)
 	}
+	<-channel
 }
